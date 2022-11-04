adadc0fe06dd80983ac9e09c5b7c9b9f4b59f8cc
Ding Chen <dchen@rcas6013.rcf.bnl.gov>
2022-11-04 11:05:58 -0400

    stardev

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-starver SL20d
+#starver SL20d
+stardev
 cons
 root4star -q -l -b analyzePico.C\(\"./st_physics_19151029_raw_1000007.picoDst.root\",\"test\",\"config_4p5GeV.txt\"\)
 echo 'Good morning/afternoon/evening/night! I love you, Ding Chen!'
